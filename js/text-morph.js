a5862c57f6a46b07cf4388387cd84fe5dab03d83
Rudra Pratap Nayak <rudrapnayak111@gmail.com>
2026-02-15 11:37:11 +0530

    Slowed morf animation

--- js/text-morph.js
+++ js/text-morph.js
@@ -5,8 +5,8 @@
 
 (function () {
     const CHARS = '!@#$%^&*()_+-={}[]|;:<>?/~`ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    const ITERATIONS_PER_CHAR = 2; // How many scramble cycles before settling
-    const FRAME_DELAY = 20; // ms per frame
+    const ITERATIONS_PER_CHAR = 4; // How many scramble cycles before settling
+    const FRAME_DELAY = 40; // ms per frame
 
     class TextScramble {
         constructor(el) {
